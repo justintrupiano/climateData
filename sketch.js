ee962d814feb0e8e5c7bc1ececd1a17c81cd51ef
justintrupiano <justintrupiano@users.noreply.github.com>
2018-12-27 20:52:41 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,23 +3,24 @@ let myStrArr;
 let yearMonth = [];
 let i = 0;
 
-let a;
-let b;
 let count = 0;
 let color = 0;
+
+let lineWidth;
+
+
+
 function preload(){
   file = loadStrings("aravg.csv");
 }
 
 function setup(){
 
-  createCanvas(1000, 1666);
-  background(51);
   splitString(file);
-  // drawLines();
-  a = 0;
-  b = 100;
+  createCanvas(yearMonth.length, yearMonth.length);
+  background(51);
   drawYear()
+
 }
 
 function splitString(thisFile){
@@ -27,45 +28,50 @@ function splitString(thisFile){
     append(yearMonth,thisFile[i].split(" "));
   }
 }
+let minMaxArr = [];
+function getMinMax(){
+
+  for (let c = 0; c <yearMonth.length; c++){
+    minMaxArr.push(parseFloat(yearMonth[c][2]));
+  }
+  let minVal = min(minMaxArr);
+  let maxVal = max(minMaxArr);
+  return([minVal, maxVal]);
+
+}
 
 
-
+function getStart(startYear, startMonth){
+  for (let c = 0; c <yearMonth.length; c++){
+    if(yearMonth[c][0]== startYear &&
+       yearMonth[c][1] == startMonth){
+         return(c)
+       }
+      }
+}
 
 function drawYear(){
   // stroke(255, 127);
   noFill();
-  // strokeWeight(2);
+  let startNum = getStart(1880, 1);
+  let minMax = getMinMax();
+  lineWidth = floor(yearMonth.length/(yearMonth.length-startNum));
+  strokeWeight(lineWidth);
+
+  for (let c = yearMonth.length-1; c > startNum; c--){
+    strokeVal = map(float(yearMonth[c][2]), minMax[0], minMax[1], 0,255);
+    stroke(strokeVal);
+    // line(0, c*lineWidth, width, c*lineWidth);
+    line(0, (yearMonth.length-c)*lineWidth, width, (yearMonth.length-c)*lineWidth);
+    // line(c+(yearMonth.length/2),0, c+(yearMonth.length/2), height);
 
 
-  for (let c = 0; c < yearMonth.length; c++){
-
-  // for (let c = yearMonth.length-1; c > 0; c--){
-  //     if (count == 0){
-  //   beginShape();
-  // }
-      // line(0, i, width, i);
-      stroke(map(float(yearMonth[c][2]), -1, 1.5, 0, 255), 127);
-      // color += float(yearMonth[c][2]);
-      // vertex(a, b + 25*(float(yearMonth[c][2])));
-      line(0, c, width, c)
-      // console.log(yearMonth[c][0]);
-
-  //     i++;
-  //     a= a + width/11;
-  //     count++;
-  //
-  //   if (count == 12){
-  //     count = 0;
-  //     console.log(color);
-  //     stroke(map(color, -8, 12.6, 0, 255));
-  //     endShape();
-  //     color = 0;
-  //     b += height/(yearMonth.length/11.75);
-  //     a = 0;
-  //   }
-  //
   }
 }
+
+
+
+
 
 function draw(){
     // stroke(map(yearMonth[i][2], -2, 2, 0, 255));
